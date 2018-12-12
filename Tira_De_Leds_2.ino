1e644f4c5161c52cb1ab7cbb183d500cef5d966e
victorcubedo7 <45770234+victorcubedo7@users.noreply.github.com>
2018-12-12 15:02:04 +0100

    Add files via upload

---
+++ Tira_De_Leds_2.ino
@@ -0,0 +1,19 @@
+//TIPO DOS, TIRA DE LEDS SE ENCIENDE Y SE APAGA DE UNO EN UNO
+int i = 6;    //PRIMER PIN
+int tiempo1 = 100; //TIEMPO DE ESPERA ms
+
+void setup() {
+
+  for (i = 6; i < 10; i++) { //i=6 (primer pin por el que empieza); i<10 (ultimo pin); i++ es el incremento
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop() {
+  for (i = 6; i < 10; i++) { //SOLO HE PUESTO 4 LEDS
+    digitalWrite(i, 1);
+    delay(tiempo1);
+    digitalWrite(i, 0);
+    delay(tiempo1);
+  }
+}
---
+++ Tira_De_Leds__1.ino
@@ -0,0 +1,22 @@
+//TIPO 1, TIRA DE LEDS SE ENCIENDEN TODOS Y SE APAGAN POCO A POCO
+int i = 6;    //PRIMER PIN
+int tiempo1 = 200; //TIEMPO DE ESPERA ms
+
+void setup() {
+
+  for (i = 6; i < 10; i++) { //i=6 (primer pin por el que empieza); i<10 (ultimo pin); i++ es el incremento
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop() {
+  for (i = 6; i < 10; i++) { //SOLO HE PUESTO 4 LEDS
+    digitalWrite(i, 1);
+    delay(tiempo1);
+  }
+   for (i = 6; i < 10; i++) { //SOLO HE PUESTO 4 LEDS
+    
+    digitalWrite(i, 0);
+    delay(tiempo1);
+  }
+}
